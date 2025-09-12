c5ac86c55da2bcdb7185d7dad7a9b425b3ccc52d
SPAWNS <1835939062@qq.com>
2025-09-12 20:55:20 +0800

    Update Init.ts

--- src/scripts/Init.ts
+++ src/scripts/Init.ts
@@ -105,6 +105,6 @@ export default () => {
     MusicList.forEach((i: any) => i.destroy());
     MusicList.length = 0;
   });
-  console.log("%c🌻 哗哗哗🌻", "color:#fff; background: linear-gradient(270deg, #18d7d3, #68b7dd, #8695e6, #986fee); padding: 8px 15px; border-radius: 8px");
+  console.log("%c❤️我爱你❤️", "color:#fff; background: linear-gradient(270deg, #18d7d3, #68b7dd, #8695e6, #986fee); padding: 8px 15px; border-radius: 8px");
   console.log("%c\u521D\u59CB\u5316\u5B8C\u6BD5.", "color: #ffffff; background: #000; padding:5px");
 }
